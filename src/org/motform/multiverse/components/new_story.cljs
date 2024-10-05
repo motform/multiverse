74dc47d9a8817f5b592d758c2bc64dfd7e67784b
motform <love.lagerkvist@protonmail.com>
2024-10-05 17:45:55 +0200

    feat: `NewStory` styling

--- src/org/motform/multiverse/components/new_story.cljs
+++ src/org/motform/multiverse/components/new_story.cljs
@@ -25,9 +25,11 @@
 (defn PromptVersions []
   (let [active @(rf/subscribe [:new-story/prompt-version])
         Toggle (partial Toggle active :new-story/prompt-version)]
-    [:section.template-toggles.h-stack.gap-quarter
-     [Toggle :prompt/v1 "v1"]
-     [Toggle :prompt/v2 "v2"]]))
+    [:section.v-stack.gap-quarter
+     [:span.toggle-label "Prompt"]
+     [:section.template-toggles.h-stack.gap-quarter
+      [Toggle :prompt/v1 "v1"]
+      [Toggle :prompt/v2 "v2"]]]))
 
 (defn submit-story []
   (rf/dispatch [:new-story/submit])
@@ -41,48 +43,48 @@
         user-message @(rf/subscribe [:new-story/user-message])
         promt-version @(rf/subscribe [:new-story/prompt-version])
         blank? (str/blank? prompt)]
-    [:section.prompt.prompt-background.v-stack.gap-full.rounded-large.shadow-large.pad-half
+    [:div.prompt-container
+     [:section.prompt.prompt-background.v-stack.gap-full.rounded-large.shadow-large.pad-half
+      [:section.v-stack.gap-full.full-width
+       [:section.v-stack.gap-half
+        [:span.prompt-label "System Message"]
+        [:textarea#system-prompt.prompt-textarea.textarea-large.rounded.shadow-large.pad-half
+         {:value system-message
+          :rows 10
+          :disabled (= promt-version :prompt/v1)
+          :on-change #(rf/dispatch [:new-story/update :new-story/system-message (.. % -target -value)])}]
+        [:p.template-tip "The system message is the message that the AI will see before generating the story.\nOpenAI claims it is weighted more heavily than the user message."]]
 
-     [:section.v-stack.gap-half
-      [:span.prompt-label "System Message"]
-      [:section.v-stack.gap-quarter
-       [:textarea#system-prompt.prompt-textarea.textarea-large.rounded.shadow-large.pad-half
-        {:value system-message
-         :rows 10
-         :disabled (= promt-version :prompt/v1)
-         :on-change #(rf/dispatch [:new-story/update :new-story/system-message (.. % -target -value)])}]
-       [:p.template-tip "The system message is the message that the AI will see before generating the story.\nOpenAI claims it is weighted more heavily than the user message."]]
+       [:section.v-stack.gap-half
+        [:span.prompt-label "User Message"]
+        [:textarea#system-prompt.prompt-textarea.textarea-large.rounded.shadow-large.pad-half
+         {:value user-message
+          :rows 6
+          :disabled (= promt-version :prompt/v1)
+          :on-change #(rf/dispatch [:new-story/update :new-story/user-message (.. % -target -value)])}]
+        [:p.template-tip "The user message is the last message the AI sees, after having been given the entire story up to this point."]]
 
-      [:span.prompt-label "User Message"]
-      [:section.v-stack.gap-quarter
-       [:textarea#system-prompt.prompt-textarea.textarea-large.rounded.shadow-large.pad-half
-        {:value user-message
-         :rows 4
-         :disabled (= promt-version :prompt/v1)
-         :on-change #(rf/dispatch [:new-story/update :new-story/user-message (.. % -target -value)])}]
-       [:p.template-tip "The user message is the last message the AI sees, after having been given the entire story up to this point."]]
+       [PromptVersions]]
 
+      [:section.v-stack.gap-full.full-width
+       [:section.v-stack.gap-half
+        [:span.prompt-label "Story Prompt"]
+        [:textarea#story-prompt.prompt-textarea.textarea-large.rounded.shadow-large.pad-half
+         {:value prompt
+          :rows 4
+          :auto-focus true
+          :on-change #(rf/dispatch [:new-story/update :new-story/prompt (.. % -target -value)])
+          :on-key-down #(when (and (or (.-metaKey %) (.-ctrlKey %))
+                                   (= (.-key %) "Enter"))
+                          (submit-story))}]]
 
-      [PromptVersions]]
+       [:section.h-stack.spaced {:style {:align-items "flex-end"}}
+        [Models]
 
-     [:section.v-stack.gap-half
-      [:span.prompt-label "Story Prompt"]
-      [:textarea#story-prompt.prompt-textarea.textarea-large.rounded.shadow-large.pad-half
-       {:value prompt
-        :rows 3
-        :auto-focus true
-        :on-change #(rf/dispatch [:new-story/update :new-story/prompt (.. % -target -value)])
-        :on-key-down #(when (and (or (.-metaKey %) (.-ctrlKey %))
-                                 (= (.-key %) "Enter"))
-                        (submit-story))}]]
-
-
-     [:section.h-stack.spaced {:style {:align-items "flex-end"}}
-      [Models]
-      [:button.rounded.shadow-medium.tab.prompt-button-submit.blurred
-       {:disabled blank?
-        :on-pointer-down #(when-not blank? (submit-story))}
-       "Explore"]]]))
+        [:button.rounded.shadow-medium.tab.prompt-button-submit.blurred
+         {:disabled blank?
+          :on-pointer-down #(when-not blank? (submit-story))}
+         "Explore"]]]]]))
 
 (defn NewStory []
   [:main.new-story.v-stack.gap-full
